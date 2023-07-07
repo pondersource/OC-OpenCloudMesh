9ed95cb3059cf2618435470b0c6624cdef4e2cd8
Reza Soltani <soltanireza65@gmail.com>
2023-07-07 09:47:21 +0330

    some refactor

--- opencloudmesh/lib/Hooks/UserHooks.php
+++ opencloudmesh/lib/Hooks/UserHooks.php
@@ -48,55 +48,10 @@ class UserHooks
 
                 foreach ($userGroups as $group) {
                     $groupId = $group->getGID();
-
-                    $getGroupSharesStmt = $this->connection->prepare("SELECT * FROM  `*PREFIX*share_external_group` WHERE `user` = ?");
-
-                    $getUserSharesStmt = $this->connection->prepare("SELECT * FROM  `*PREFIX*share_external_group` WHERE `user` = ?");
-
-                    $groupShares = $getGroupSharesStmt->execute([$groupId]) ? $getGroupSharesStmt->fetchAll() : [];
-                    $userShares = $getUserSharesStmt->execute([$userId]) ? $getUserSharesStmt->fetchAll() : [];
-
-                    $openShares = array_diff($userShares, $groupShares);
-
-                    $openShares = array_filter($groupShares, function ($groupShare) use ($userShares) {
-                        return !array_filter($userShares, function ($userShare) use ($groupShare) {
-                            return $userShare['parent'] == $groupShare['id'];
-                        });
-                    });
-
-                    foreach ($openShares as $share) {
-                        
-                        $shareFolder = \OCA\Files_Sharing\Helper::getShareFolder();
-                        $mountPoint = \OCP\Files::buildNotExistingFileName($shareFolder, $share['name']);
-                        $mountPoint = \OC\Files\Filesystem::normalizePath($mountPoint);
-                        $mountpoint_hash = \md5($mountPoint);
-
-                        $query = $this->connection->prepare("
-                            INSERT INTO oc_share_external_group
-                            (`parent`, `remote`,`remote_id`, `share_token`, `password`, `name`, `owner`, `user`, `mountpoint`, `mountpoint_hash`, `accepted`)
-                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                        ");
-                        $query->execute([
-                            $share['id'],
-                            $share['remote'],
-                            $share['remote_id'],
-                            $share['share_token'],
-                            $share['password'],
-                            $share['name'],
-                            $share['owner'],
-                            $userId,
-                            $mountPoint,
-                            $mountpoint_hash,
-                            1,
-                        ]);
-                    }
+                    
+                    $this->externalManager->acceptRemoteGroupShares($groupId, $userId);
                 }
             });
         }
-    }
-
-    private function acceptRemoteGroupSharesAutomatically($user)
-    {
-
     }
 }
